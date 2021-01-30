e0bb9933e211f798dcd66cee5dac100740c6f604
SergFateev <fateev-si@ya.ru>
2021-01-31 00:01:31 +0300

    Add fields for create a post on a home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :users, only: [:show, :edit, :update]
+
+  resources :posts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
