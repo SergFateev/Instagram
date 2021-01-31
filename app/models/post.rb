976dcc9ebd945b4800686862b76b1eb129a4114a
SergFateev <fateev-si@ya.ru>
2021-01-31 20:50:49 +0300

    Correct handler of image presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  include ImageUploader::Attachment(:image)
+#  validate :image_presence
 
-  has_one_attached :image
-
-  validate :image_presence
-
-  def image_presence
-    errors.add(:image, "can't be blank") unless image.attached?
-  end
+  # def image_presence
+  #   errors.add(:image, "can't be blank") unless image.attached?
+  # end
 end
