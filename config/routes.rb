f3618b31fc0a2cae0e182364bdcfa932e09cca1d
SergFateev <fateev-si@ya.ru>
2021-02-01 12:28:20 +0300

    Add show action

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update]
 
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
