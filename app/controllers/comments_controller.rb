9e10cbf825d66e98398c17fd02f4a8cccce4bd1c
SergFateev <fateev-si@ya.ru>
2021-02-08 20:54:59 +0300

    Correct add comment form, show comments after submit (local: true)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,6 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    @comment.user = current_user.include(user)
 
     redirect_to post_path(@post)
   end
@@ -13,5 +12,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :user_id, :post_id)
   end
-  
+
 end
